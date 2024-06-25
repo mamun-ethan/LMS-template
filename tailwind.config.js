f9b99dacd47ba506f8172838d802e42234bd0ec8
mamun ethan <mamunsikder73375@gmail.com>
2024-06-25 11:48:24 +0600

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    fontFamily: {
+      hind: ["Hind Siliguri", "sans-serif"],
+    },
+
+    extends: {},
+  },
+  plugins: [],
+};
