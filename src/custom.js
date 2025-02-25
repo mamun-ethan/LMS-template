76b0e53ea2fb744d9a444465201f6edc865ddb84
mamun ethan <mamunsikder73375@gmail.com>
2025-02-25 21:46:21 +0600

    course filter update

--- src/custom.js
+++ src/custom.js
@@ -1,22 +1,3 @@
-// course filtler
-document.addEventListener("DOMContentLoaded", (event) => {
-  filterCourses("web-development");
-});
-
-function filterCourses(category) {
-  const menuButtons = document.querySelectorAll(".menu-btn");
-  menuButtons.forEach((button) => button.classList.remove("active"));
-
-  const activeButton = document.getElementById(`${category}-btn`);
-  activeButton.classList.add("active");
-
-  const courses = document.querySelectorAll(".course-box");
-  courses.forEach((course) => (course.style.display = "none"));
-
-  const selectedCourses = document.querySelectorAll(`.${category}`);
-  selectedCourses.forEach((course) => (course.style.display = "block"));
-}
-
 // skills filter
 document.addEventListener("DOMContentLoaded", (event) => {
   filterSkills("skill-web-development");
